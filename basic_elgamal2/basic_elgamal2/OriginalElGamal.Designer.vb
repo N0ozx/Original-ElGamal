8da629ded34bea64e3b55b5f0b6cde9fa434d726
N0ozx <158080692+N0ozx@users.noreply.github.com>
2024-01-29 20:18:47 +0300

    Add files via upload

---
+++ basic_elgamal2/basic_elgamal2/OriginalElGamal.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class OriginalElGamal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        decrypt = New Button()
+        plain_text = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Encrypt = New Button()
+        cipher_text = New TextBox()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        decrypted_text = New TextBox()
+        Label13 = New Label()
+        SuspendLayout()
+        ' 
+        ' decrypt
+        ' 
+        decrypt.Location = New Point(612, 396)
+        decrypt.Name = "decrypt"
+        decrypt.Size = New Size(96, 23)
+        decrypt.TabIndex = 1
+        decrypt.Text = "decrypt"
+        decrypt.UseVisualStyleBackColor = True
+        ' 
+        ' plain_text
+        ' 
+        plain_text.Location = New Point(143, 121)
+        plain_text.Multiline = True
+        plain_text.Name = "plain_text"
+        plain_text.Size = New Size(193, 116)
+        plain_text.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(168, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(108, 50)
+        Label1.TabIndex = 6
+        Label1.Text = "Alice"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(628, 37)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(92, 50)
+        Label2.TabIndex = 7
+        Label2.Text = "Bob"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(80, 124)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(57, 15)
+        Label3.TabIndex = 8
+        Label3.Text = "Plain Text"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(766, 124)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(103, 15)
+        Label4.TabIndex = 9
+        Label4.Text = "Prime Number (p)"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(766, 163)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(100, 15)
+        Label5.TabIndex = 10
+        Label5.Text = "Primitive Root (g)"
+        ' 
+        ' Encrypt
+        ' 
+        Encrypt.Location = New Point(193, 396)
+        Encrypt.Name = "Encrypt"
+        Encrypt.Size = New Size(96, 23)
+        Encrypt.TabIndex = 0
+        Encrypt.Text = "Encrypt"
+        Encrypt.UseVisualStyleBackColor = True
+        ' 
+        ' cipher_text
+        ' 
+        cipher_text.Location = New Point(143, 276)
+        cipher_text.Multiline = True
+        cipher_text.Name = "cipher_text"
+        cipher_text.ReadOnly = True
+        cipher_text.Size = New Size(193, 114)
+        cipher_text.TabIndex = 4
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(71, 189)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(0, 15)
+        Label8.TabIndex = 15
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(71, 327)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(66, 15)
+        Label9.TabIndex = 16
+        Label9.Text = "Cipher Text"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(766, 222)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(65, 15)
+        Label10.TabIndex = 18
+        Label10.Text = "Private Key"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(766, 251)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(62, 15)
+        Label11.TabIndex = 19
+        Label11.Text = "Public Key"
+        ' 
+        ' decrypted_text
+        ' 
+        decrypted_text.Location = New Point(559, 276)
+        decrypted_text.Multiline = True
+        decrypted_text.Name = "decrypted_text"
+        decrypted_text.ReadOnly = True
+        decrypted_text.Size = New Size(201, 114)
+        decrypted_text.TabIndex = 22
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(766, 327)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(85, 15)
+        Label13.TabIndex = 23
+        Label13.Text = "Decrypted Text"
+        ' 
+        ' form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Orange
+        ClientSize = New Size(880, 458)
+        Controls.Add(Label13)
+        Controls.Add(decrypted_text)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(cipher_text)
+        Controls.Add(plain_text)
+        Controls.Add(decrypt)
+        Controls.Add(Encrypt)
+        Name = "form1"
+        Text = "Basic_Elgamal"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents decrypt As Button
+    Friend WithEvents plain_text As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Encrypt As Button
+    Friend WithEvents cipher_text As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents decrypted_text As TextBox
+    Friend WithEvents Label13 As Label
+End Class
---
+++ basic_elgamal2/basic_elgamal2/OriginalElGamal.vb
@@ -0,0 +1,215 @@
+﻿Imports System.Numerics
+Imports System.Drawing
+Imports System.IO
+Imports System.Security.Cryptography
+
+Public Class OriginalElGamal
+    Dim e, p, g, privateKey As BigInteger
+    Dim plainText As String
+    Dim ciphertext As New List(Of BigInteger)
+    Dim bp As New List(Of BigInteger)
+    Private Sub Encrypt_Click(sender As Object, t As EventArgs) Handles Encrypt.Click
+        Dim index As Integer = 0
+        plainText = plain_text.Text
+
+        'Convert the plaintext to ASCII values
+        Dim asciiValues As New List(Of BigInteger)
+        For Each c As Char In plainText
+            asciiValues.Add(BigInteger.Parse(AscW(c).ToString()))
+        Next
+
+
+        Dim primeString As String = GeneratePrime()
+        p = BigInteger.Parse(primeString)
+
+        g = 2
+        'primitive_root.Text = g.ToString()
+
+        privateKey = GeneratePrivateKey(p)
+
+        'Calculate the public key
+        e = BigInteger.ModPow(g, privateKey, p)
+        ' Dim publicKeyString As String = $"{p}{g}{e}"
+        '  public_key.Text = publicKeyString.ToString()
+
+        ' Encrypt the ASCII values
+
+
+        Dim display_ciphertext As New List(Of BigInteger)
+        Do While bp.Count < Len(plainText)
+            If bp.Count = Len(plainText) Then
+                Exit Do
+            End If
+
+            Dim randomInteger As BigInteger = GenerateRandomInteger(p)
+            Dim c1 As BigInteger = BigInteger.ModPow(g, randomInteger, p)
+            bp.Add(c1)
+
+            Dim sk As BigInteger = BigInteger.ModPow(e, randomInteger, p)
+
+
+
+            Dim c2 As BigInteger = (asciiValues(index) * sk) Mod p
+
+            ciphertext.Add(c2)
+            display_ciphertext.Add(c2 Mod 65536)
+
+            index += 1
+
+        Loop
+        Dim encryptedString As String = ""
+        For Each value As BigInteger In display_ciphertext
+            encryptedString &= ChrW(BigInteger.Parse(value.ToString()))
+        Next
+        display_ciphertext.Clear()
+        ' Display the encrypted string
+        cipher_text.Text = encryptedString
+
+    End Sub
+
+    Private Sub Decrypt_Click(sender As Object, e As EventArgs) Handles decrypt.Click
+        Dim Index As Integer = 0
+        Dim decryptedAsciiValues As New List(Of BigInteger)
+        For Each c1 As BigInteger In bp
+
+            Dim sk As BigInteger = BigInteger.ModPow(c1, privateKey, p)
+
+            Dim inversec1equa = ModInverse(sk, p)
+
+            Dim asciiValue As BigInteger = (ciphertext(Index) * inversec1equa) Mod p
+
+            decryptedAsciiValues.Add(asciiValue)
+
+            Index += 1
+        Next
+        ' Calculate decrypted ASCII values
+
+
+
+        ' Convert ASCII values to characters
+        Dim decryptedString As String = ""
+        For Each asciiValue As BigInteger In decryptedAsciiValues
+            decryptedString &= ChrW(BigInteger.Parse(asciiValue.ToString()))
+        Next
+
+        ciphertext.Clear()
+        bp.Clear()
+        decrypted_text.Text = decryptedString
+    End Sub
+    Private Function ModInverse(a As BigInteger, m As BigInteger) As BigInteger
+        Dim m0 = m
+        Dim x0 As BigInteger = 0
+        Dim x1 As BigInteger = 1
+
+        While a > 1
+            Dim q As BigInteger
+            Dim t As BigInteger = 0
+
+            q = BigInteger.DivRem(a, m, t)
+            a = m
+            m = t
+            t = x0
+            x0 = x1 - q * x0
+            x1 = t
+        End While
+
+        If x1 < 0 Then
+            x1 += m0
+        End If
+
+        Return x1
+    End Function
+
+    Private Function GenerateRandomInteger(prime As BigInteger) As BigInteger
+        ' Specify the desired bit length (1000 bits)
+        Dim bitLength As Integer = 1000
+
+        ' Calculate the number of bytes needed based on the specified bit length
+        Dim numBytes As Integer = (bitLength + 7) \ 8
+
+        ' Create a Cryptographically Secure Pseudorandom Number Generator (CSPRNG)
+        Using rng As RandomNumberGenerator = RandomNumberGenerator.Create()
+            ' Generate a random byte array
+            Dim randomBytes(numBytes - 1) As Byte
+
+            ' Fill the byte array with random bytes
+            rng.GetBytes(randomBytes)
+
+            ' Ensure the generated BigInteger is within the specified bit length
+            randomBytes(randomBytes.Length - 1) = randomBytes(randomBytes.Length - 1) And 127
+            Dim randomInteger As New BigInteger(randomBytes)
+
+            ' Ensure that 1 <= randomInteger < prime - 2
+            While randomInteger <= 1 OrElse randomInteger >= prime - 2
+                ' Generate a new random byte array
+                rng.GetBytes(randomBytes)
+
+                ' Ensure the generated BigInteger is within the specified bit length
+                randomBytes(randomBytes.Length - 1) = randomBytes(randomBytes.Length - 1) And 127
+                randomInteger = New BigInteger(randomBytes)
+            End While
+
+
+            Return randomInteger
+        End Using
+    End Function
+
+    Private Function GeneratePrivateKey(prime As BigInteger) As BigInteger
+        ' Specify the desired bit length (1024 bits)
+        Dim bitLength As Integer = 1024
+
+        ' Calculate the number of bytes needed based on the specified bit length
+        Dim numBytes As Integer = (bitLength + 7) \ 8
+
+        ' Create a Cryptographically Secure Pseudorandom Number Generator (CSPRNG)
+        Using rng As RandomNumberGenerator = RandomNumberGenerator.Create()
+            ' Generate a random byte array
+            Dim randomBytes(numBytes - 1) As Byte
+
+            ' Fill the byte array with random bytes
+            rng.GetBytes(randomBytes)
+
+            ' Ensure the generated BigInteger is within the specified bit length
+            randomBytes(randomBytes.Length - 1) = randomBytes(randomBytes.Length - 1) And 127
+            Dim privateKey As New BigInteger(randomBytes)
+
+            ' Ensure that 1 <= randomInteger < prime - 2
+            While privateKey <= 1 OrElse privateKey >= prime - 1
+                ' Generate a new random byte array
+                rng.GetBytes(randomBytes)
+
+                ' Ensure the generated BigInteger is within the specified bit length
+                randomBytes(randomBytes.Length - 1) = randomBytes(randomBytes.Length - 1) And 127
+                privateKey = New BigInteger(randomBytes)
+            End While
+
+
+            Return privateKey
+        End Using
+    End Function
+    Private Function GeneratePrime() As String
+        Dim process As New Process()
+        process.StartInfo.FileName = "cmd.exe"
+        process.StartInfo.UseShellExecute = False
+        process.StartInfo.RedirectStandardInput = True
+        process.StartInfo.RedirectStandardOutput = True
+        process.Start()
+
+        Dim streamWriter As StreamWriter = process.StandardInput
+        Dim streamReader As StreamReader = process.StandardOutput
+        streamWriter.WriteLine("openssl prime -generate -bits 1024")
+        streamWriter.WriteLine("exit")
+
+        Dim result As String = streamReader.ReadToEnd()
+        Dim lines As String() = result.Split(New String() {Environment.NewLine}, StringSplitOptions.None)
+        For Each line As String In lines
+            If line.Length > 0 AndAlso Char.IsDigit(line(0)) Then
+                Return line
+            End If
+        Next
+
+        Return Nothing
+    End Function
+
+
+End Class
